d713a256e035bc0b72a491c54596aa5718510b2d
Basil Suter <git@nadar.io>
2019-03-22 17:48:12 +0100

    add basic idea for master sdk

---
+++ src/Event.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Heartbeat\Master;
+
+use luya\headless\ActiveEndpoint;
+
+/**
+ * Event Model.
+ * 
+ * The event object contains the meta informations about an event, date informations (as events
+ * can have multiple dates) are stored in EventDate model.
+ * 
+ * Therefore data is retrieved from API trough EventDate.
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
+ */
+class Event extends ActiveEndpoint
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getEndpointName()
+    {
+        return '{{%events}}';
+    }
+
+    public $title;
+    public $description;
+    public $price;
+    public $age;
+    public $website;
+    public $ticket_link;
+    public $flyer;
+
+    private $_pois = [];
+
+    public function setPois(array $pois)
+    {
+        $this->_pois = $pois;
+    }
+
+    public function getPois()
+    {
+        return Poi::iterator($this->_pois);
+    }
+}
---
+++ src/EventDate.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Heartbeat\Master;
+
+use luya\headless\ActiveEndpoint;
+
+/**
+ * Event Date Active Endpoint.
+ * 
+ * Contains the Event Date informations.
+ * 
+ * The meta informations about events are stored in Event.
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
+ */
+class EventDate extends ActiveEndpoint
+{
+    public $id;
+    public $start_timestamp;
+    public $end_timestamp;
+    public $extra_text;
+    public $origin;
+    public $impressions;
+    public $impressions_hot;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getEndpointName()
+    {
+        return '{{%eventdates}}';
+    }
+
+    public static function find()
+    {
+        return parent::find()->setContentProcessor(function($content) {
+            return isset($content['items']) ? $content['items'] : $content;
+        });
+    }
+
+    private $_event;
+
+    public function setEvent(array $event)
+    {
+        $this->_event = $event;
+    }
+
+    public function getEvent()
+    {
+        return new Event($this->_event);
+    }
+}
